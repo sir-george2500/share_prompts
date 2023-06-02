0366ae276ee073e52c7fffae921b0fa18a2dbbbb
George S Mulbah II <gsmulbah2500@gmail.com>
2023-06-02 02:37:27 +0000

    Added value mobile navigation view

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -2,18 +2,31 @@
 import Link from "next/link";
 import Image from "next/image";
 import{useState,useEffect}from 'react'
-import {signOut,signIn,useSession,getProvider} from 'next-auth/react'
+import {signOut,signIn,useSession,getProviders} from 'next-auth/react'
 
 const Nav = () => {
    const isUserLoggedIn = true;
+
+   const [provider,setProvider] = useState(null);
+
+   const [toggleDropDown,setToggleDropDown]=useState(false);
+
+   useEffect(()=>{
+    const setProviders = async ()=>{
+      const response = await getProviders();
+
+      setProviders(response)
+    }
+    setProviders();
+   },[])
   return (
     <nav className="flex-between w-full mb-16 pt-3">
      <Link href='/' className='flex gap-2 flex-center'>
         <Image
           src='/assets/images/logo.svg'
           alt='logo'
-          width={30}
-          height={30}
+          width={37}
+          height={37}
           className='object-contain'
         />
         <p className='logo_text'>Prompt-Space</p>
@@ -30,21 +43,94 @@ const Nav = () => {
             SingOut
           </button>
           <Link href="/profile">
-          <Image
-          src='/assets/icons/profile_avatar.png'
-          alt='logo'
-          width={30}
-          height={30}
-          className='object-contain'
-        />
+              <Image
+              src='/assets/icons/profile_avatar.png'
+              alt='profile'
+              width={30}
+              height={30}
+              className='object-contain'
+            />
           </Link>
-         </div>
+         </div>   
         ):(
          <>
-
+         {
+          provider && Object.
+          values(providers).
+          map((provider) => (
+            <button 
+            type="button"
+            key={provider.name}
+            onClick={()=>signIn(provider.id)} className="black_btn">
+             Sign In
+            </button>
+           )
+          )
+         }
          </>
         )}
        </div>
+     {/* Mobile Navigation*/}
+     <div className="sm:hidden flex relative">
+     {isUserLoggedIn ?(
+         <div className="flex ">
+          <Link href="/">
+              <Image
+              src='/assets/icons/profile_avatar.png'
+              width={30}
+              height={30}
+              className='object-contain' 
+              alt="profile"
+              onClick={()=> setToggleDropDown((prev) =>!prev)}
+            />
+          </Link>
+          {toggleDropDown && (
+            <div className="dropdown">
+              <Link
+               href="/profile"
+               className="dropdown_link"
+               onClick={()=>setToggleDropDown(false)}
+              >
+               My Profile
+              </Link>
+              <Link
+               href="/create-prompt"
+               className="dropdown_link"
+               onClick={()=>setToggleDropDown(false)}
+              >
+               Create Prompt
+              </Link>
+              <button
+               type="button"
+               onClick={()=>{
+                setToggleDropDown(false)
+                signOut();
+               }}
+               className="mt-5 w-full black_btn"
+              >
+              Sign Out
+              </button>
+            </div>
+          )}
+         </div>   
+        ):(
+         <>
+         {
+          provider && Object.
+          values(providers).
+          map((provider) => (
+            <button 
+            type="button"
+            key={provider.name}
+            onClick={()=>signIn(provider.id)} className="black_btn">
+             Sign In
+            </button>
+           )
+          )
+         }
+         </>
+        )}
+     </div>
     </nav>
   )
 }
