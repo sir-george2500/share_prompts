a7b440723587b7c003056618d25724a632ec70b1
George S Mulbah II <gsmulbah2500@gmail.com>
2023-06-03 00:54:01 +0000

    Add MongoDB successfully

--- app/layout.jsx
+++ app/layout.jsx
@@ -12,6 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+        <Provider>
         <div className='main'>
           <div className='gradient' />
         </div>
@@ -19,6 +20,7 @@ export default function RootLayout({ children }) {
         <Nav/>
         {children}
       </main>
+        </Provider>
       </body>
     </html>
   )
---
+++ models/user.js
@@ -0,0 +1,25 @@
+import {Schema,model,models} from 'mongoose';
+
+const UserSchema = new Schema({
+    email:{
+        type:String,
+        unique:[true,'Email already exists!'],
+        required:[true ,'Email is required!'],
+    },
+    username:{
+        type:String,
+        required:[true,'Username is required!'],
+        match:[/^(?=.{8,20}$)(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z0-9]{8,20}$/,
+        "Username invalie , it should contain 8-20 alphanumeric letters and be unique!"]
+
+    },
+    image:{
+        type:String,
+    }
+});
+
+const User = models.User || model("User" , UserSchema);
+
+
+
+export default User;
