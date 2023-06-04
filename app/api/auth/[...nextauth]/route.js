47dc74e44747e5507d1aaa6a8280c3bf2d553fa6
George S Mulbah II <gsmulbah2500@gmail.com>
2023-06-04 16:37:46 +0000

    added fix for authentication Provider bug

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,6 @@
-import NextAuth from "next-auth";
-import GoogleProvider from 'next-auth/providers/google'
-import { connectToDB } from "@utils/database";
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+import { connectToDB } from '@utils/database';
 import User from '@models/user';
 
 // Establish database connection
@@ -10,35 +10,37 @@ const handler = NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET
-    })
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
   ],
 
   // Optional: Define additional callbacks
   callbacks: {
-    async signIn({ profile, email, account }) {
+    // Session callback
+    async session({ session }) {
+        const sessionUser = await User.findOne({
+            email: session.user.email
+        })
+
+        session.user.id = sessionUser.id.toString();
+
+        return session;
+    },
+    async signIn({ profile }) {
       // Check if a user already exists
       const userExists = await User.findOne({ email: profile.email });
 
       if (!userExists) {
         await User.create({
           email: profile.email,
-          username: profile.name.replace(" ", "").toLowerCase(),
-          image: profile.picture
+          username: profile.name.replace(/ /g, '').toLowerCase(),
+          image: profile.picture,
         });
       }
 
       return true; // Allow sign in
     },
-
-    async session({ session, user }) {
-      const sessionUser = await User.findOne({ email: user.email });
-
-      session.user.id = sessionUser.id.toString();
-
-      return session;
-    }
-  }
+  },
 });
 
 export { handler as GET, handler as POST };
