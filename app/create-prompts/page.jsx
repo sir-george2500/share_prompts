1fba0ff957a675f35844104083a98bd5b23eb0a3
George S Mulbah II <gsmulbah2500@gmail.com>
2023-06-04 22:27:53 +0000

    Designing the form begin

--- app/create-prompts/page.jsx
+++ app/create-prompts/page.jsx
@@ -1,8 +1,28 @@
-
+'use client'
+import { useSession } from "next-auth/react";
+import { useState } from "react";
+import Router from "next/router";
+import Form from "@components/Form";
 
 const CreatePrompt = () =>{
+   const [submitting,setSubmitting]=useState(false);
+   const [post, setPost] = useState({
+    prompt:'',
+    tag :''
+   })
+
+   const createPrompt = async (e) => {
+
+   }
   return(
-    <div>Create Prompt</div>
+
+    <Form  
+     type="Create Post"
+     post={post}
+     setPost={setPost}
+     submitting={submitting}
+     handleSubmitting={createPrompt}
+    />
   )
 }
 
--- components/Form.jsx
+++ components/Form.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
-
-const Form = () => {
+import Link from 'next/link';
+const Form = ({
+  type,
+  post,
+  setPost,
+  submitting,
+  handleSubmitting
+}) => {
   return (
-    <div>Form</div>
+    <section className='w-full max-w-full flex-start flex-col'>
+     <h1 className="head_text text-left">{type}</h1>
+    </section>
   )
 }
 
