1c0f64eca21b4d97114301e52dac45583186b23a
George S Mulbah II <gsmulbah2500@gmail.com>
2023-06-06 00:44:09 +0000

    add PromptCard from the database

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,12 +1,46 @@
 'use client'
 import { useState, useEffect} from 'react';
 import PromptCard from './PromptCard';
+
+//handle the tage Click 
+
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className='mt-16 prompt_layout'>
+      {data.map((post) => (
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  );
+};
+
+
 const Feed = () => {
   const [searchText, setSearchText] = useState('');
+  const [posts,setPosts] = useState([])
 
   const handleSearchChange = (e) => {
-
+    
   }
+
+  //fetch the data here 
+
+  const  fetchPosts = async () => {
+    const response = await fetch('/api/prompt');
+    const data = await response.json();
+    setPosts(data);
+  }
+
+  //the data when the feed component load
+  useEffect(() => {
+    fetchPosts();
+  },[])
+
+
   return (
     <section className='feed'>
       <form className='relative w-full flex-center '>
@@ -19,6 +53,12 @@ const Feed = () => {
          className="search_input peer"
        />
       </form>
+
+      <PromptCardList 
+      data = {posts}
+      handleTagClick={()=>{}}
+      
+      />
     </section>
   )
 }
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+"use client";
+import { useState } from "react";
+import Image from "next/image";
+import { useSession } from "next-auth/react";
+import { usePathname, useRouter } from "next/navigation";
 
-const PromptCard = () => {
+const PromptCard = ({post, handleTagClick, handleEdit , handleDetele}) => {
+
+  const [copied,setCopied] = useState("");
   return (
-    <div>PromptCard</div>
+    <div className="prompt_card">
+      <div className="flex justify-between item-start gap-5">
+       <div className="flex-1 flex justify-start item-center gap-3 cursor-pointer">
+
+        <Image 
+         src={post.creator.image}
+         width={40}
+         height={40}
+         className="roudnded-full object-contain"
+        />
+       <div className="flex flex-col">
+         <h3 className="font-satoshi font-semibold text-gray-900">{post.creator.username}</h3>
+         <p className="font-inter text-sm text-gray-900">{post.creator.email}</p>
+       </div>
+       </div>
+       {/** div for the copy button */}
+        <div className="copy_btn" onClick={()=>{}}>
+         <Image 
+          src={copied===post.PromptCard
+          ?'/assets/icons/tick.svg'
+          :'/assets/icons/copy.svg'
+          }
+          width={12}
+          height={12}
+         />
+        </div>
+      </div>
+      <p className="my-4 font-satoshi text-sm text-gray-700">{post.prompt}</p>
+      <p 
+      className="font-inter text-sm blue_gradient cursor-pointer"
+      onClick={()=> handleTagClick && handleTagClick(post.tag)}
+      >#{post.tag}</p>
+    </div>
   )
 }
 
